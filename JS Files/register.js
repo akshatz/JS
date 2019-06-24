2aaa41636a7f6e25d929568b668b859c9d0589eb
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2019-06-24 22:55:04 +0530

    Update register.js

--- JS Files/register.js
+++ JS Files/register.js
@@ -1,13 +1,20 @@
-function checkforblank(){
-    var a=0;
-    var pwd= document.login.pass.value;
+var pwd = document.login.pass.value;
+var lname = document.login.lname.value;
+var email=  document.login.email.value;
+var uname = document.login.uname.value;
+var fname = document.login.fname.value;
+var gender = document.login.gender.value;
+var address = document.login.address.value;
+var JSON1;
+function check_for_blank(){
+    var pwd = document.login.pass.value;
     var lname = document.login.lname.value;
     var email=  document.login.email.value;
     var uname = document.login.uname.value;
     var fname = document.login.fname.value;
     var gender = document.login.gender.value;
     var address = document.login.address.value;
-    if (email==''){
+        if (email==''){
         document.getElementById('email').style.borderColor="red";
     }
     if (pwd==''){
@@ -22,64 +29,89 @@ function checkforblank(){
     if (lname ==''){
         document.getElementById('lname').style.borderColor="red";
     }
-    if(a==gender.length){
+    if((gender.length)==''){
         document.getElementById('radio').style.borderColor="red";
-        return false;
     }
     if (address ==''){
         document.getElementById('address').style.borderColor="red";
     }
     if (fname==''||lname==''||address==''||pwd==''||email=='' ){
-        return false;    
+        return false;
     }
 
+}
 function address_validate(){
     var address_regex= '^[#.0-9a-zA-Z\s,-]+$';
-    var address_result=document.getElementById('address').value.match(address_regex);
-    if(!(address_result==true)){
-        alert("Pls enter proper address");
-    }
+    var add =document.getElementById('address').value;
+    var address_result=add.match(address_regex);
 }
 function fname_validate(){
     var fname =document.getElementById('fname').value;
-    var name_regex ='[a-zA-z]';
+    let name_regex ='[a-zA-z]';
     var fname_result= fname.match(name_regex);
-    if(fname_result){
-        alert("Correct Fname");
+    if((fname_result)){
+        return;
     }
 }
+
 function lname_validate(){
-    
-    var lname =document.getElementById('lname').value;
-    var name_regex ='[a-zA-z]';
-    var lname_result= lname.match(name_regex);
-    if(lname_result){
-        alert("Correct lname");
+    let name_regex ='[a-zA-z]';
+    var lname = document.getElementById('lname').value;
+    var lname_result = lname.match(name_regex);
+    document.write(lname_result)
+    if((lname_result)){
+        return;
     }
 }
 
 function validation_password(){
-    alert("Plz enter with 1 character, 1 lower case 1 upper case, and 1 number  min 8 characters");
     //password validation
     var pwd=document.login.pass.value;
     var passwdregex='^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])$'
     var password_result = pwd.match(passwdregex);
-    if(password_result&&(pwd.length > 6)){
-        alert("Plz enter with 1 character, 1 lower case 1 upper case, and 1 number  min 8 characters");
-        return false;
+    if((password_result)){
+        return;
     }
-    console.log("in password_result");
-    if(password_result.length<=8)
-        alert("Password must be of 8 characters. Please re-enter your Password");
-        return false;
-    }
- 
+}
 function ValidateEmail() {
     //email validation
     var email= document.login.email.value;
     var emailregex='[a-zA-Z0-9\\+\\.\\_\\%\\-\\]{1,256}\\@[a-zA-z0-9][a-zA-z0-9\\-]{0, 64}(\\[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25})';
     var emailresult = email.match(emailregex);
-    if (emailresult){
+    if((emailresult)){
+        return;
+    }
+}
+function ValidateUsername(){
+    var user =document.login.uname.value;
+    var userreg = [A-Za-z];
+    if(user.match(userreg)){
         return false;
     }
 }
+//Local Storage
+var userdetails = new Array ("lname", "pwd", "address", "uname", "email", "fname", "gender");
+var obj = {
+    firstname: fname.value,
+    address : address.value,
+    password : pass.value,
+    gender: gender.value,
+    email:email.value,
+    username:uname.value,
+    lastname: lname.value,
+};
+var myJSON= JSON.stringify(obj);
+// Store
+if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("last Name","lname.value");
+    localStorage.setItem("first name", "fname.value");
+    localStorage.setItem("password", "pass.value");  
+    localStorage.setItem("address", "address.value"); 
+    localStorage.setItem("uname", "uname.value");
+    localStorage.setItem("email", "email.value");
+    localStorage.setItem("gender", "gender.value");
+}
+function OnSubmit(){
+    return window.location.href = 'http://www.google.com'; 
+}
+JSON1 = JSON.parse(myJSON);
